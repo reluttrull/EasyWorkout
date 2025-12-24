fe7c0eefd7eb230e0cf4398f0d781d82ca787a3e
Ryan Luttrull <reluttrull@gmail.com>
2025-12-24 14:27:34 -0600

    Start creating do-workout form

--- easyworkout.client/src/app/do-workout/do-workout.ts
+++ easyworkout.client/src/app/do-workout/do-workout.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, OnInit, signal, inject } from '@angular/core';
+import { ReactiveFormsModule, FormBuilder, FormGroup } from '@angular/forms';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { WorkoutsService } from '../workouts/workouts.service';
 import { WorkoutResponse } from '../model/interfaces';
@@ -6,11 +7,12 @@ import { OrderByPipe } from '../pipes/order-by-pipe';
 
 @Component({
   selector: 'app-do-workout',
-  imports: [OrderByPipe],
+  imports: [ReactiveFormsModule, OrderByPipe],
   templateUrl: './do-workout.html',
   styleUrl: './do-workout.css',
 })
 export class DoWorkout implements OnInit {
+  fb = inject(FormBuilder);
   id: string | null = null;
   workoutGoal = signal<WorkoutResponse>({
     id: '',
@@ -20,6 +22,9 @@ export class DoWorkout implements OnInit {
     notes: '',
     exercises: []
   });
+  form = this.fb.group({
+    sets: this.fb.array<FormGroup>([])
+  });
   
   constructor(private route: ActivatedRoute, private workoutsService: WorkoutsService) {}
 
@@ -27,6 +32,35 @@ export class DoWorkout implements OnInit {
     this.route.paramMap.subscribe((params: ParamMap) => {
       this.id = params.get('id');
     });
-    this.workoutsService.get(this.id??'').subscribe(w => this.workoutGoal.set(w));
+    this.workoutsService.get(this.id??'').subscribe(w => {
+      this.workoutGoal.set(w);
+      this.initForm();
+    });
+  }
+
+  get setsArray() {
+    return this.form.controls.sets;
+  }
+
+  initForm() {
+    this.form.controls.sets.clear();
+
+    for (const exercise of this.workoutGoal().exercises) {
+      for (const set of exercise.exerciseSets) {
+        this.form.controls.sets.push(
+          this.fb.group({
+            setId: [set.id],
+            weight: [set.weight ?? null],
+            reps: [set.reps ?? null],
+            duration: [set.duration ?? null]
+          })
+        );
+      }
+    }
+  }
+
+  submit() {
+    const completedSets = this.form.value.sets;
+    console.log(completedSets);
   }
 }

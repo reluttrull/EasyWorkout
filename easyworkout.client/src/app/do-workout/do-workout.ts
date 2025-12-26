aa5291fbbdfce08b28f3898aa8865a4e8ffc31f4
Ryan Luttrull <reluttrull@gmail.com>
2025-12-26 10:27:20 -0600

    Collect form data from completed workout

--- easyworkout.client/src/app/do-workout/do-workout.ts
+++ easyworkout.client/src/app/do-workout/do-workout.ts
@@ -7,6 +7,7 @@ import { OrderByPipe } from '../pipes/order-by-pipe';
 
 @Component({
   selector: 'app-do-workout',
+  providers: [OrderByPipe],
   imports: [ReactiveFormsModule, OrderByPipe],
   templateUrl: './do-workout.html',
   styleUrl: './do-workout.css',
@@ -22,11 +23,14 @@ export class DoWorkout implements OnInit {
     notes: '',
     exercises: []
   });
+  orderedExercises: WorkoutResponse['exercises'] = [];
+
   form = this.fb.group({
-    sets: this.fb.array<FormGroup>([])
+    exercises: this.fb.array<FormGroup>([])
   });
+
   
-  constructor(private route: ActivatedRoute, private workoutsService: WorkoutsService) {}
+  constructor(private route: ActivatedRoute, private workoutsService: WorkoutsService, private orderByPipe: OrderByPipe) {}
 
   ngOnInit(): void {
     this.route.paramMap.subscribe((params: ParamMap) => {
@@ -34,33 +38,57 @@ export class DoWorkout implements OnInit {
     });
     this.workoutsService.get(this.id??'').subscribe(w => {
       this.workoutGoal.set(w);
+      console.log(this.workoutGoal());
       this.initForm();
     });
   }
 
-  get setsArray() {
-    return this.form.controls.sets;
+  get exercisesArray() {
+    return this.form.controls.exercises;
   }
+initForm() {
+  this.exercisesArray.clear();
 
-  initForm() {
-    this.form.controls.sets.clear();
+  const orderedExercises = this.orderByPipe.transform(
+    this.workoutGoal().exercises,
+    'addedDate',
+    'asc'
+  );
 
-    for (const exercise of this.workoutGoal().exercises) {
-      for (const set of exercise.exerciseSets) {
-        this.form.controls.sets.push(
-          this.fb.group({
-            setId: [set.id],
-            weight: [set.weight ?? null],
-            reps: [set.reps ?? null],
-            duration: [set.duration ?? null]
-          })
-        );
-      }
+  for (const exercise of orderedExercises) {
+    const setsArray = this.fb.array<FormGroup>([]);
+
+    exercise.exerciseSets = this.orderByPipe.transform(
+      exercise.exerciseSets,
+      'setNumber',
+      'asc'
+    );
+
+    for (const set of exercise.exerciseSets) {
+      setsArray.push(
+        this.fb.group({
+          setId: [set.id],
+          weight: [null],
+          reps: [null],
+          duration: [null]
+        })
+      );
     }
+
+    this.exercisesArray.push(
+      this.fb.group({
+        exerciseId: [exercise.id],
+        sets: setsArray
+      })
+    );
   }
+
+  this.orderedExercises = orderedExercises;
+}
+
 
   submit() {
-    const completedSets = this.form.value.sets;
-    console.log(completedSets);
+    console.log(this.form.value.exercises);
   }
+
 }

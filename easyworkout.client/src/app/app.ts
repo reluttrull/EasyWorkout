bf1c4665ca1cb8ae70df09823d1358f72992e44f
Ryan Luttrull <reluttrull@gmail.com>
2026-01-01 09:49:24 -0600

    Add responsive nav menu

--- easyworkout.client/src/app/app.ts
+++ easyworkout.client/src/app/app.ts
@@ -2,13 +2,14 @@ import { Component, signal, computed, inject } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatMenuModule } from '@angular/material/menu';
 import { AuthService } from './auth/auth.service';
 import { AccountService } from './account/account.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, MatIconModule, MatButtonModule],
+  imports: [RouterOutlet, MatIconModule, MatButtonModule, MatMenuModule ],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })

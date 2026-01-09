867d582aae175934d13ee06136e3002b9eeb14a9
Ryan Luttrull <reluttrull@gmail.com>
2026-01-09 14:17:43 -0600

    Start hooking up to chart.js

--- easyworkout.client/src/app/reports/reports.ts
+++ easyworkout.client/src/app/reports/reports.ts
@@ -1,16 +1,40 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { BaseChartDirective } from 'ng2-charts';
+import { ChartConfiguration, ChartOptions, ChartData, ChartType } from 'chart.js';
 import { ReportsService } from './reports.service';
 import { WeightUnit } from '../model/enums';
-import { TotalVolumeReportRequest, TotalVolumeReportResponse } from '../model/interfaces';
+import { TotalVolumeReportRequest, TotalVolumeReportResponse, DataPointResponse } from '../model/interfaces';
 
 @Component({
   selector: 'app-reports',
-  imports: [],
+  imports: [BaseChartDirective],
   templateUrl: './reports.html',
   styleUrl: './reports.css',
 })
 export class Reports {
+  weightUnit = signal(WeightUnit.Pounds.toString());
   reportsService = inject(ReportsService);
+  isChartLoaded = signal(false);
+  
+  public lineChartOptions: ChartOptions = {
+    responsive: true,
+    scales: {
+      y: {
+        title: {
+          display: true,
+          text: this.weightUnit()
+        }
+      }
+    }
+  };
+  public lineChartType: ChartType = 'line';
+  public lineChartData: ChartData<'line'> = {
+    labels: [] as string[],
+    datasets: [
+      { data: [] }
+    ]
+  };
+  public lineChartLegend = false;
 
   fetchTotalVolume() {
     // default to pounds for now
@@ -23,8 +47,31 @@ export class Reports {
     this.reportsService.getTotalVolumeReport(request).subscribe({
         next: (response) => {
           console.log(response);
+          this.isChartLoaded.set(true);
+          this.weightUnit.set(response.weightUnit);
+          this.displayData(response);
         },
         error: err => console.error(err)
       });
+  }
+
+  displayData(response:{dataPoints: DataPointResponse[]}) {
+    let newDataArr:number[] = [];
+    let newLabelsArr:string[] = [];
+    response.dataPoints.forEach(d => {
+    // 1. Add the new label
+    newLabelsArr = [...newLabelsArr, new Date(d.completedDate).toISOString().split('T')[0]];
+
+    // 2. Add the new data point to the first dataset
+    newDataArr = [...newDataArr, d.totalVolume];
+      });
+
+    // 3. Clone the entire datasets array and reassign to trigger change detection
+    this.lineChartData = {
+      labels: newLabelsArr,
+      datasets: [
+      { data: newDataArr }
+      ]
+    };
   }
 }
--- easyworkout.client/src/main.ts
+++ easyworkout.client/src/main.ts
@@ -5,10 +5,12 @@ import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { AppComponent } from './app/app';
 import { routes } from './app/app-routes';
 import { authInterceptor } from './app/core/auth.interceptor';
+import { provideCharts, withDefaultRegisterables } from 'ng2-charts';
 
 bootstrapApplication(AppComponent, {
   providers: [
     provideRouter(routes),
-    provideHttpClient(withInterceptors([authInterceptor]))
+    provideHttpClient(withInterceptors([authInterceptor])),
+    provideCharts(withDefaultRegisterables())
   ]
 });

e79da56f2d2bd833510dd6ce9d287bd6570ef032
Ryan Luttrull <reluttrull@gmail.com>
2026-01-11 15:06:18 -0600

    Narrow Playwright projects a bit

--- easyworkout.client/playwright.config.ts
+++ easyworkout.client/playwright.config.ts
@@ -46,12 +46,8 @@ export default defineConfig({
 
     /* Test against mobile viewports. */
     {
-      name: 'Mobile Chrome',
-      use: { ...devices['Pixel 5'] },
-    },
-    {
       name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] },
+      use: { ...devices['iPhone 14'] },
     }
   ],
 
